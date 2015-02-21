c2dbd6d678a66713e030da18da64e3b12ac1bba1
Martin Honermeyer <maze@strahlungsfrei.de>
2015-02-21 16:41:53 +0100

    Allow cross-origin access (this time for real)
    /cc @milafrerichs)

--- server.rb
+++ server.rb
@@ -1,6 +1,5 @@
 require 'sinatra/base'
 require "sinatra"
-require 'sinatra/cross_origin'
 require 'data_mapper'
 require './lib/buerger_buero/waiting_citizen'
 require './config/config'
@@ -9,6 +8,8 @@ module Buergerbuero
   class App < Sinatra::Base
     before do
       content_type :json
+      headers 'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => ['OPTIONS', 'GET']
     end
 
     DataMapper::setup(:default, ENV["DATABASE"])
